465440e803b650bcb0d307b392bd1fe1e313287d
Zhilong Liu <lzl200102109@gmail.com>
2014-11-30 21:28:18 -0800

    finished the plot module in measure_during_drive.py

--- python/measure_during_drive.py
+++ python/measure_during_drive.py
@@ -1,29 +1,77 @@
-import zc_id
+import zc_id, time, matplotlib, csv
 from Xbee import *
 from LCMBot import *
-
-import time
+matplotlib.use('GTKAgg')
+import matplotlib.pyplot as plt
 
 if __name__=='__main__':
+
+    # initialize Zumy, XBee, and file handles.
     rid = zc_id.get_id()
     r = LCMBot('{0}/base_cmd'.format(rid))
-    
-    file_name = raw_input("Please input the file name: ")
-    f = open(file_name + ".csv", "w")
-
     xb = XbRssi('/dev/ttyUSB0')
+    file_name = raw_input("Please input the file name: ")
+    OUTPUT_NAME = file_name + '.csv'
+    f = open(OUTPUT_NAME, "w")
 
+    # time
     start_time = time.time()
     driving_time = 5
-    i = 0
-
+    
+    # ===============================
+    # take measurements
+    # ===============================
     r.drive(0.18,0.18)
-   
+    i = 0
     while time.time()-start_time < driving_time: 
-        rssi, rssi_list = xb.get_max_rssi()
-        f.write("Position #" + str(i) + "rssi = " + str(rssi) + " " + str(rssi_list) + "\n")
+        rssi_max, rssi_list = xb.get_max_rssi()
+        rssi_avg = mean(rssi_list)
+        rssi_med = median(rssi_list)
+        f.write(str(i) + ", " + str(rssi_max) + ", " + str(rssi_med) + ", " + str(rssi_avg)  + "\n")
         i = i + 1
-
     r.stop()
-    f.close()    
+    f.close()
+
+    # ===============================
+    # plot
+    # ===============================
+
+    # initialize plot
+    plt.title('Test_plot')
+    plt.xlabel('Postion')
+    plt.ylabel('RSSI (dB)')
+
+    # initialize variables
+    t = []
+    rssi_max = []
+    rssi_med = []
+    rssi_avg = []
+
+    # read csv file
+    f = open(OUTPUT_NAME,'rb')
+    input = csv.reader(f, quoting=csv.QUOTE_NONNUMERIC)
+    rssi_data = [[item for number, item in enumerate(row) if item and (0 <= number <= 3)] for row in input]
+    for i in range(len(rssi_data)):
+        if i == 0:
+            t.append(0);
+            rssi_max.append(rssi_data[i][0])
+            rssi_med.append(rssi_data[i][1])
+            rssi_avg.append(rssi_data[i][2])
+        else:
+            t.append(rssi_data[i][0]);
+            rssi_max.append(rssi_data[i][1])
+            rssi_med.append(rssi_data[i][2])
+            rssi_avg.append(rssi_data[i][3])
+    f.close()
+
+    # plot
+    plt.plot(t, rssi_max, color='red', linewidth=2.0, linestyle='-', label='$RSSII_{max}$')
+    plt.savefig(file_name + '.png')
+    del t, rssi_max, rssi_med, rssi_avg
+    plt.show()
+
+
+
+
+    
 
